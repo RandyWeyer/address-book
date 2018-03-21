b9ddbb1979d5c3261398858bb2962aabd3b5bea7
CJ Rhone and Randy Weyer <cj@>
2018-03-21 16:02:59 -0700

    Add dynamic cards based on form information submitted.

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $("#newContact").submit(function(event) {
+
+        event.preventDefault();
+
+    var firstInput = $("input#first-name").val();
+    var lastInput = $("input#last-name").val();
+    var birthdayInput= $("input#birthday").val();
+    var genderInput = $("input#gender").val();
+    var streetInput = $("input#street").val();
+    var cityInput = $("input#city").val();
+    var stateInput = $("input#state").val();
+    var zipCodeInput = $("input#zip-code").val();
+
+    $(".first-name").text(firstInput);
+    $(".last-name").text(lastInput);
+    $(".birthday").text(birthdayInput);
+    $(".gender").text(genderInput);
+    $(".street").text(streetInput);
+    $(".city").text(cityInput);
+    $(".state").text(stateInput);
+    $(".zip-code").text(zipCodeInput);
+
+    // $("#contact-card").append("<div id=\"contact-card\">"));
+    // $("#contact-card").append("<div class=\"well\"><h4>Name: <span class=\"" + first-name + "\"></span> <span class=\"" + last-name "\"></span>"));
+    $("#results").append("<div class='col-md-2 contact-card'><h4>Name: " + firstInput + " " + lastInput+ "</h4><h4>Birthday: "+ birthdayInput +"</h4><h4>Gender: " + genderInput + "</h4><h4>Address: "+ streetInput + ", " + stateInput + " " + zipCodeInput + "</h4></div>");
+  });
+});
